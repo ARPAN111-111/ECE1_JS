bf02585fe1255ebb8a65bd330fc72b01ea2eb9fa
ARPAN111-111 <arpan1218171@gmail.com>
2026-02-25 10:12:38 +0530

    second

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ num = true;
 console.log(typeof(Boolean));
 var number
 console.log(typeof(nest));
+/* symbol*/
+const sym1=symbol(4)
+console.log(sym1);
+const sym2=symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
