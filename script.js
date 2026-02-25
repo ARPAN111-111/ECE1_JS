ed2224cfca21ae49082ccbf0a1640576521973b5
ARPAN111-111 <arpan1218171@gmail.com>
2026-02-25 09:51:32 +0530

    message

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello, world");
+var num=10;
+console.log(num);
+var name="ARPAN SHARMA"
+console.log(typeof(name));
+num = true;
+console.log(typeof(Boolean));
+var number
+console.log(typeof(nest));
