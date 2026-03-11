fd9e513454876b079d6e15a4ccac0ccdd13ddd7d
ARPAN111-111 <arpan1218171@gmail.com>
2026-03-11 09:44:56 +0530

    class 2

---
+++ classs2.js
@@ -0,0 +1,39 @@
+/*let count =0;
+console.log("starting loop"+"<br/>");
+for(count=0; count<10; count++){
+    console.log("current count:"+count);
+    console.log("<br/>")
+}
+console.log("loop stopped!");
+
+// for in loop
+const person={
+    Fname:"ARPAN",
+    Iname:"SHARMA",
+    age:25,
+};
+for(x in person){
+    console.log("person details;"+x+";"+person[x])
+}
+//Conditional Opeator
+var age =25;
+if(age<19){
+    console.log("<b> you are eligible</b>")
+}
+else{
+    console.log("<b>You are not eligible</b>")
+}
+//Switch Case
+var grade='A';
+document.write("entering switch case"+"<br/>");
+switch(grade){
+    case 'A':document.write("good bache</br");
+    break;
+    case 'B':document.write("shi h</br");
+    break;
+    case 'C':document.write("thik hi h</br");
+    break;
+    case 'D':document.write("jyada acha n</br");
+    default:document.write("bekar</br")
+}*/
+//
